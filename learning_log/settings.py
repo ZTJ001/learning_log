1dcbd6b50a1491bfc63288f59a4267e709d30f7d
ZTJ001 <130875640+ZTJ001@users.noreply.github.com>
2024-08-11 10:55:02 +0800

    Update settings-delete-key.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-00dl2!bnsi3a)bq0rva$!m%e2nbi69^ije@csvfd0ma_6=@k*#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # 关闭调试模式
