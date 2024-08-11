86ea76b0078abea18b9f1fe3d9f72d237035ab92
ZTJ001 <baixun9951@163.com>
2024-08-11 14:47:13 +0800

    改

--- learning_log/.ipynb_checkpoints/settings-checkpoint.py
+++ learning_log/.ipynb_checkpoints/settings-checkpoint.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os  
+#1dcbd6b50a1491bfc63288f59a4267e709d30f7d
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-00dl2!bnsi3a)bq0rva$!m%e2nbi69^ije@csvfd0ma_6=@k*#'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default_secret_key_for_development')  
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# 关闭调试模式
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
 
-ALLOWED_HOSTS = []
+# 允许的服务器
+ALLOWED_HOSTS = ['*']
 
+# 静态文件收集目录
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 # Application definition
 
