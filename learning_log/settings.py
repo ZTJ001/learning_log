b36e8af9fbdd2f6f054ecb0d4f27b2ad6d042123
ZTJ001 <130875640+ZTJ001@users.noreply.github.com>
2024-08-11 12:59:43 +0800

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-<<<<<<< HEAD
-import os
-=======
 import os  
->>>>>>> 1dcbd6b50a1491bfc63288f59a4267e709d30f7d
+#1dcbd6b50a1491bfc63288f59a4267e709d30f7d
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-6ebihw(&+_s9(!@d5s3-aq1p8u=rn1&3pf7f$c%h5dfu-6^=0h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # 关闭调试模式
-DEBUG = False
+DEBUG = True
 
 # 允许的服务器
 ALLOWED_HOSTS = ['*']
